160a6cd7013b32b0e88234af6640430bbd933997
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2017-03-21 13:59:04 +0100

    StarDunks

--- StarDunk_databaseHandler.php
+++ StarDunk_databaseHandler.php
@@ -1,8 +1,9 @@
 <?php
 
-	require('ass016_databaseHandlerClass.php');
+	require('StarDunk_databaseHandlerClass.php');
 
-	$handler = new dbHandler('localhost', 'crudDB', 'root', '');
+	$handler = new dbHandler('localhost', 'stardunks', 'root', '');
+	echo '<header><link rel="stylesheet" type="text/css" href="StarDunk.css"></header>';
 
 		// Insert data
 		// $sql = "INSERT INTO `crudDB`.`users` VALUES (NULL, 'Firstname', 'Lastname', 'UserCity');";
@@ -48,16 +49,18 @@
 		}
 
 		//Table showen
-		$sql = "SELECT * FROM `users` LIMIT 0, 19";
+		$sql = "SELECT * FROM `products` LIMIT 0, 19";
 		$res = $handler->readData($sql);
-		echo "<table border='1'>";
+		echo "<div class=row>";
+		echo "<a href='createProduct.php'><button>Create New Product</button></a>";
+		echo "<table class='col-12' border='1'>";
 		foreach ($res as $row)
 		{
 			foreach ($row as $key => $val)
 			{
 				echo "<th>$key</th>";
 			}
-				echo "<th>Update</th>";
+				echo "<th>The boy can play</th>";
 			break;
 		}
 
@@ -67,33 +70,34 @@
 					echo "<tr>";
 		   foreach ($row as $key => $val)
 			 {
-		       echo "<td><input value='".$val."' name=".$key."></td>";
+		       echo "<td>$val</td>";
 		   }
-			 echo "<td><input type='submit' /></td>";
+			 echo "<td><input type='submit' value='Read' /><input type='submit' value='Update' /><input type='submit' value='Delete' /></td>";
 		   echo "</tr>";
 			 echo "</form>";
 
 		}
-			 echo "<form action='' method='POST'><tr><td><input type='text' readonly/></td><td><input type='text' name='Firstname' placeholder='Firstname'/></td><td><input type='text' name='Lastname' placeholder='Lastname'/></td><td><input type='text' name='UserCity' placeholder='UserCity'/></td><td><input type='submit' name='insert' value='Insert' /></td></tr></form>";
-			 //  DEZE INSERT
-			//  foreach($res as $row)
-			//    {
-			//     echo '<tr><td colspan=5 style="text-align:center;">INSERT HERE:</td></tr>';
-			//     echo '<form action="" method="POST"><tr>';
-			//     foreach($row as $key => $val)
-			//     {
-			//      if($key == 'user_id')
-			//      {
-			//       echo '<td><input type="text" disabled></td>';
-			//      }
-			//      else
-			//      {
-			//       echo '<td><input type="text" name="'. $key . '" value="'. $key .'"></td>';
-			//      }
-			//
-			//     }
-			//     echo  '<td><input type="submit"  name="insert" value="Insert"></td></tr></form>';
-			//     break;
-			//    }
+echo "</table>";
+echo "</div>"
+
+
+	// foreach($res as $row)
+  // {
+  //  echo '<form action="" method="POST"><tr>';
+  //  foreach($row as $key => $val)
+  //  {
+  //   if($key == 'user_id')
+  //   {
+  //    echo '<td><input type="text" disabled></td>';
+  //   }
+  //   else
+  //   {
+  //    echo '<td><input type="text" name="'. $key . '" value="'. $key .'"></td>';
+  //   }
+	//
+  //  }
+  //  echo  '<td><input type="submit"  name="insert" value="Insert"></td></tr></form>';
+  //  break;
+  // }
 
 ?>
---
+++ StarDunk_databaseHandlerClass.php
@@ -0,0 +1,90 @@
+<?php
+
+	class dbHandler{
+
+		private $server;
+		private $dbName;
+		private $user;
+		private $password;
+
+		function __construct($server, $dbName, $user, $password)
+		{
+			$this->server = $server;
+			$this->dbName = $dbName;
+			$this->user = $user;
+			$this->password = $password;
+
+			try
+			{
+				$this->conn = new PDO("mysql:host=$this->server;dbname=$this->dbName", $this->user, $this->password);
+				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e)
+			{
+				echo 'Connection failed: ' . $e->getMessage();
+			}
+		}
+
+		function createData($sql)
+		{
+			try
+			{
+				$this->conn->exec($sql);
+				return $this->conn->lastInsertId();
+			}
+			catch(PDOException $e)
+			{
+				return "Create failed: " . $e->getMessage();
+			}
+		}
+
+		function deleteData($sql)
+		{
+			try
+			{
+				$this->conn->exec($sql);
+				return true;
+			}
+			catch(PDOException $e)
+			{
+				return "Delete failed: " . $e->getMessage();
+			}
+		}
+
+		function updateData($sql)
+		{
+			try
+			{
+				$stmt = $this->conn->prepare($sql);
+				$stmt->execute();
+				return $stmt->rowCount();
+			}
+			catch(PDOException $e)
+			{
+				return "Update failed: " . $e->getMessage();
+			}
+		}
+
+		function readData($sql)
+		{
+			try
+			{
+				$stmt = $this->conn->prepare($sql);
+				$stmt->execute();
+				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				return $result;
+			}
+			catch(PDOException $e)
+			{
+				return "Error: " . $sql . "<br>" . $e->getMessage();
+			}
+		}
+
+		function __destruct()
+		{
+			$this->conn = null;
+		}
+
+	}
+
+?>
